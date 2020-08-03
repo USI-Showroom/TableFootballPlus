a3303a532139163628c98d2c2f27949e8f6fc585
Stefano Taillefert <stefano.taille@gmail.com>
2020-08-03 16:06:55 +0200

    Update lights, add serial comm

--- tableFootballPlus/tableFootballPlus.ino
+++ tableFootballPlus/tableFootballPlus.ino
@@ -1,6 +1,9 @@
 /* 
  *  Code for the table football at USI, Lugano
- *  by Stefano Taillefert, August 2020
+ *  developed by Stefano Taillefert in August 2020
+ *  for Arduino Uno.
+ *  
+ *  See https://github.com/USI-Showroom/TableFootballPlus
  *  
  *  Libraries used: FastLED (https://github.com/FastLED/FastLED)
  */
@@ -8,6 +11,7 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 1
+#define DEBUG
 
 //Port numbers
 #define BUZZER 0
@@ -26,6 +30,11 @@ int scoreYellow = 0;
 CRGB leds[NUM_LEDS];
 
 void setup() {
+  #ifdef DEBUG
+    Serial.begin(115200);
+    Serial.print("Starting up...");
+  #endif
+  
   pinMode(BUZZER, OUTPUT);
   pinMode(SENSOR_WHITE, INPUT);
   pinMode(SENSOR_YELLOW, INPUT);
@@ -44,63 +53,111 @@ void setup() {
   delay(150);
   tone(BUZZER, 600, 200);
 
-  leds[0] = CRGB::Green;
-  FastLED.show();
+  setDefaultLights();
+
+  #ifdef DEBUG
+    Serial.println(" done.");
+  #endif
 }
 
-void scoreEffectWhite() {
-  scoreWhite++;
+/*
+ * Updates values and produces sound/light effects
+ * if the white player scores
+ */
+void scoreEffectWhite() {  
+  ++scoreWhite;
+
+  #ifdef DEBUG
+    Serial.println("White scored!");
+    Serial.print("Score: ");
+    Serial.print(scoreWhite);
+    Serial.print("-");
+    Serial.println(scoreYellow);
+  #endif
   
   tone(BUZZER, 450, 400);
   delay(150);
   tone(BUZZER, 470, 400);
 
-  leds[0] = CRGB::White;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::White;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Green;
-  FastLED.show();
-  delay(500);
+  delayedLoop(CRGB::White, 500);
 }
 
-void scoreEffectYellow() {
-  scoreYellow++;
+/*
+ * Updates values and produces sound/light effects
+ * if the yellow player scores
+ */
+void scoreEffectYellow() {  
+  ++scoreYellow;
+
+  #ifdef DEBUG
+    Serial.println("Yellow scored!");
+    Serial.print("Score: ");
+    Serial.print(scoreWhite);
+    Serial.print("-");
+    Serial.println(scoreYellow);
+  #endif
   
   tone(BUZZER, 400, 400);
   delay(150);
   tone(BUZZER, 450, 400);
 
-  leds[0] = CRGB::Yellow;
+  delayedLoop(CRGB::Yellow, 500);
+}
+
+/*
+ * Changes the color of the whole strip at once.
+ */
+void changeColor(CRGB newColor) {
+  for (int i = 0; i < NUM_LEDS; ++i) {
+    leds[i] = newColor;
+  }
   FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Yellow;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Green;
-  FastLED.show();
-  delay(500);
+}
+
+/*
+ * Produces a ring effect with the given color and delay,
+ * then return to default effect.
+ */
+void delayedLoop(CRGB newColor, int interval) {
+  leds[0] = newColor;
+  delay(interval);
+  for (int i = 1; i < NUM_LEDS; ++i) {
+    leds[i] = newColor;
+    leds[i - 1] = CRGB::Black;
+    delay(interval);
+  }
+  leds[NUM_LEDS - 1] = CRGB::Black;
+  delay(interval);
+  setDefaultLights();
+}
+
+/*
+ * Sets the default idle effect.
+ */
+void setDefaultLights() {
+  changeColor(CRGB::Green);
+  //TODO fancier effect
 }
 
 void loop() {
+  
+  //Handle light change
+  
   if (digitalRead(BTN_LIGHTS) == 1) {
+    #ifdef DEBUG
+      Serial.println("Toggling lights effects\n");
+    #endif
     //Toggle lights
     lightsOn = !lightsOn;
   }
 
   if (lightsOn) {
-    //TODO turn on or off lights
+    setDefaultLights();
   } else {
-    
+    changeColor(CRGB::Black);
   }
+
+  //Handle score change
 
   if (digitalRead(BTN_SCORE_WHITE_UP) == 1 || digitalRead(SENSOR_WHITE) == 1) {
     scoreEffectWhite();
@@ -108,5 +165,13 @@ void loop() {
 
   if (digitalRead(BTN_SCORE_YELLOW_UP) == 1 || digitalRead(SENSOR_YELLOW) == 1) {
     scoreEffectYellow();
+  }
+
+  if (digitalRead(BTN_SCORE_WHITE_DOWN) == 1) {
+    --scoreWhite;
+  }
+
+  if (digitalRead(BTN_SCORE_YELLOW_DOWN) == 1) {
+    --scoreYellow;
   }
 }
