e3d0c20af79d709b5362b03339523a6969da27b1
Stefano Taillefert <stefano.taille@gmail.com>
2020-08-11 13:16:18 +0200

    Change reset btn, cleanup

--- tableFootballPlus/tableFootballPlus.ino
+++ tableFootballPlus/tableFootballPlus.ino
@@ -9,6 +9,7 @@
  */
 
 #include <FastLED.h>
+#include <avr/wdt.h>
 
 #define DEBUG
 #define DEFAULT_COLOR CRGB::Green
@@ -23,15 +24,16 @@
 #define LED_MODE GRB
 
 //Port numbers
-#define BTN_SCORE_YELLOW_DOWN 2
-#define BTN_SCORE_YELLOW_UP 3
-#define BTN_SCORE_WHITE_DOWN 4
-#define BTN_SCORE_WHITE_UP 5
-#define BTN_LIGHTS 6
-#define SENSOR_WHITE 7
-#define SENSOR_YELLOW 8
-#define LED_DATA 9
-#define BUZZER 10
+#define BTN_RESET 2
+#define BTN_SCORE_YELLOW_DOWN 3
+#define BTN_SCORE_YELLOW_UP 4
+#define BTN_SCORE_WHITE_DOWN 5
+#define BTN_SCORE_WHITE_UP 6
+#define BTN_LIGHTS 7
+#define SENSOR_WHITE 8
+#define SENSOR_YELLOW 9
+#define LED_DATA 10
+#define BUZZER 11
 
 bool lightsOn = true;
 int scoreWhite = 0;
@@ -44,15 +46,16 @@ void setup() {
     Serial.begin(115200);
     Serial.print("Starting up...");
   #endif
-  
-  pinMode(BUZZER, OUTPUT);
+
+  pinMode(BTN_RESET, INPUT);
+  pinMode(BTN_SCORE_YELLOW_DOWN, INPUT);
+  pinMode(BTN_SCORE_YELLOW_UP, INPUT);
+  pinMode(BTN_SCORE_WHITE_DOWN, INPUT);
+  pinMode(BTN_SCORE_WHITE_UP, INPUT);
+  pinMode(BTN_LIGHTS, INPUT);
   pinMode(SENSOR_WHITE, INPUT);
   pinMode(SENSOR_YELLOW, INPUT);
-  pinMode(BTN_SCORE_WHITE_UP, INPUT);
-  pinMode(BTN_SCORE_WHITE_DOWN, INPUT);
-  pinMode(BTN_SCORE_YELLOW_UP, INPUT);
-  pinMode(BTN_SCORE_YELLOW_DOWN, INPUT);
-  pinMode(BTN_LIGHTS, INPUT);
+  pinMode(BUZZER, OUTPUT);  
 
   FastLED.addLeds<LED_STRIP, LED_DATA, LED_MODE>(leds, NUM_LEDS);
 
@@ -71,46 +74,6 @@ void setup() {
 
 
 /*
- * Updates values and produces sound/light effects
- * if the white player scores.
- */
-void scoreEffectWhite() {  
-  ++scoreWhite;
-
-  #ifdef DEBUG
-    Serial.println("White scored!");
-    Serial.print("Score: ");
-    Serial.print(scoreWhite);
-    Serial.print("-");
-    Serial.println(scoreYellow);
-  #endif
-  
-  playScoreTone();
-  delayedLoop(CRGB::White, 100);
-}
-
-
-/*
- * Updates values and produces sound/light effects
- * if the yellow player scores.
- */
-void scoreEffectYellow() {  
-  ++scoreYellow;
-
-  #ifdef DEBUG
-    Serial.println("Yellow scored!");
-    Serial.print("Score: ");
-    Serial.print(scoreWhite);
-    Serial.print("-");
-    Serial.println(scoreYellow);
-  #endif
-  
-  playScoreTone();
-  delayedLoop(CRGB::Yellow, 100);
-}
-
-
-/*
  * Plays a tone when one of the players scores.
  */
 void playScoreTone() {
@@ -125,6 +88,24 @@ void playScoreTone() {
     tone(BUZZER, 450, 200);
     delay(130);
     tone(BUZZER, 550, 800);
+    delay(150);
+  #endif
+}
+
+
+/*
+ * Plays a tone when a goal is removed.
+ */
+void playDownTone() {
+  #ifdef DEBUG
+    tone(BUZZER, 350, 100);
+    delay(150);
+  #else
+    tone(BUZZER, 600, 400);
+    delay(150);
+    tone(BUZZER, 500, 200);
+    delay(150);
+    tone(BUZZER, 400, 200);
     delay(150);
   #endif
 }
@@ -189,7 +170,7 @@ void loop() {
   
   if (digitalRead(BTN_LIGHTS) == 1) {
     #ifdef DEBUG
-      Serial.println("Toggling lights effects\n");
+      Serial.println("Toggling lights effects");
     #endif
     
     lightsOn = !lightsOn;
@@ -200,23 +181,73 @@ void loop() {
   //Handle score change
 
   if (digitalRead(BTN_SCORE_WHITE_UP) == 1 || digitalRead(SENSOR_YELLOW) == 1) {
-    scoreEffectWhite();
+    ++scoreWhite;
+
+    #ifdef DEBUG
+      Serial.println("White scored!");
+      Serial.print("Score: ");
+      Serial.print(scoreWhite);
+      Serial.print("-");
+      Serial.println(scoreYellow);
+    #endif
+    
+    playScoreTone();
+    delayedLoop(CRGB::White, 100);
   }
 
   if (digitalRead(BTN_SCORE_YELLOW_UP) == 1 || digitalRead(SENSOR_WHITE) == 1) {
-    scoreEffectYellow();
+    ++scoreYellow;
+
+    #ifdef DEBUG
+      Serial.println("Yellow scored!");
+      Serial.print("Score: ");
+      Serial.print(scoreWhite);
+      Serial.print("-");
+      Serial.println(scoreYellow);
+    #endif
+    
+    playScoreTone();
+    delayedLoop(CRGB::Yellow, 100);
   }
 
   if (digitalRead(BTN_SCORE_WHITE_DOWN) == 1) {
     if (scoreWhite > 0) {
       --scoreWhite;
+      playDownTone();
+
+      #ifdef DEBUG
+        Serial.println("Decreasing white score");
+        Serial.print("Score: ");
+        Serial.print(scoreWhite);
+        Serial.print("-");
+        Serial.println(scoreYellow);
+      #endif
     }    
   }
 
   if (digitalRead(BTN_SCORE_YELLOW_DOWN) == 1) {
     if (scoreYellow > 0) {
       --scoreYellow;
+      playDownTone();
+
+      #ifdef DEBUG
+        Serial.println("Decreasing yellow score");
+        Serial.print("Score: ");
+        Serial.print(scoreWhite);
+        Serial.print("-");
+        Serial.println(scoreYellow);
+      #endif
     }    
+  }
+
+  if (digitalRead(BTN_RESET) == 1) {
+    #ifdef DEBUG
+      Serial.println("Reset");
+    #endif
+    //To reset the board, we set the watchdog to 15ms and then busy wait.
+    //Sounds like a hack but apparently that's the way ¯\_(ツ)_/¯
+    wdt_enable(WDTO_15MS);
+    while(1) {}
   }
 
   delay(200);
